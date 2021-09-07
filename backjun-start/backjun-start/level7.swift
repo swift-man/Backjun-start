48271151f4da8ffb6f6701fb1290c18508cbd709
jiniopening <jiniopening@naver.com>
2021-09-07 13:25:20 +0900

    [Feat] Level 7

---
+++ backjun-start/backjun-start/level7.swift
@@ -0,0 +1,32 @@
+//
+//  level7.swift
+//  backjun-start
+//
+//  Created by SwiftMan on 2021/09/07.
+//
+
+import Foundation
+/*
+ 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+ 
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+ */
+func level7() {
+  let num = Int(readLine()!)!
+  
+  for i in 1 ..< num {
+    print(String(repeating: " ", count: num - i) + String(repeating: "*", count: i * 2 - 1))
+  }
+  
+  for i in 0 ..< num {
+    print(String(repeating: " ", count: i) + String(repeating: "*", count: (num * 2 - 1) - i * 2))
+  }
+}
--- backjun-start/backjun-start/main.swift
+++ backjun-start/backjun-start/main.swift
@@ -7,4 +7,4 @@
 
 import Foundation
 
-level6()
+level7()
